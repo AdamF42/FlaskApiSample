0f5c3177d9d08dfed6255a09746d067b862cc871
Adamo Fapohunda <adamofapohunda@gmail.com>
2019-03-09 19:30:31 +0100

    Added docker file

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 companies = [
-    {
-        'id': 1,
-        'name': u'SuperDelivery',
-        'availability': True
-    },
-    {
-        'id': 2,
-        'name': u'FastDelivery',
-        'availability': False
-    }
+    
+        {
+            'id': 1,
+            'name': u'SuperDelivery',
+            'availability': True
+        },
+        {
+            'id': 2,
+            'name': u'FastDelivery',
+            'availability': False
+        }
+    
 ]
+
 
 @app.route('/delivery/get_availability/<city>', methods=['GET'])
 def get_availability(city):
-    return jsonify(companies), 201
+    return jsonify(companies), 200
 
 
 if __name__ == '__main__':
